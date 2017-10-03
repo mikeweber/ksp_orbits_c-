d46e726a4647febb498edcb8cf79cbd420cd8f13
Mike Weber <mike.weber@govdelivery.com>
2017-10-02 20:14:23 -0500

    Add a few more Ship attributes

--- models/ship.cpp
+++ models/ship.cpp
@@ -3,13 +3,18 @@
 Ship::Ship() {
 };
 
-Ship::Ship(CelestialBody* _parent, string _name, Velocity _vel, Position _pos, Prograde _pro, Heading _heading) {
-  parent  = _parent;
-  name    = _name;
-  vel     = _vel;
-  pos     = _pos;
-  pro     = _pro;
-  heading = _heading;
+Ship::Ship(CelestialBody* _parent, string _name, Velocity _vel, Position _pos, Prograde _pro, Heading _heading, double _thrust, double _mass) {
+  parent       = _parent;
+  name         = _name;
+  vel          = _vel;
+  pos          = _pos;
+  pro          = _pro;
+  heading      = _heading;
+  maxThrust    = _thrust;
+  mass         = _mass;
+  throttle     = 0.0;
+  fuelConsumed = 0.0;
+  fuelRate     = 0.0;
 };
 
 GravitationalParameter Ship::getParentMu() {
@@ -18,4 +23,35 @@ GravitationalParameter Ship::getParentMu() {
 
 string Ship::getParentName() {
   return parent->name;
+};
+
+void Ship::setThrottle(double _throttle) {
+  if (_throttle > 1) _throttle = 1;
+  if (_throttle < 0) _throttle = 0;
+
+  throttle = _throttle;
+};
+
+double Ship::getAcceleration() {
+  return getThrust() / getMass();
+};
+
+double Ship::getThrust() {
+  return throttle * maxThrust;
+};
+
+void Ship::consumeFuel(double consumptionRate, double timespan) {
+  fuelConsumed += consumptionRate * timespan * throttle;
+};
+
+double Ship::getMass() {
+  return mass - fuelConsumed * 0.1; // 0.1kg/unit is the density of Xenon
+};
+
+void Ship::setFuelRate(double rate) {
+  fuelRate = rate;
+};
+
+double Ship::getFuelRate() {
+  return fuelRate;
 };
--- models/ship.h
+++ models/ship.h
@@ -13,14 +13,26 @@ class Ship:public CelestialBody {
     CelestialBody* parent;
   public:
     Ship();
-    Ship(CelestialBody*, string, Velocity, Position, Prograde, Heading);
+    Ship(CelestialBody*, string, Velocity, Position, Prograde, Heading, double, double);
     GravitationalParameter getParentMu();
     string getParentName();
+    void setThrottle(double);
+    double getAcceleration();
+    double getThrust();
+    void consumeFuel(double, double);
+    double getMass();
+    void setFuelRate(double);
+    double getFuelRate();
 
   private:
     Velocity vel;
     Prograde pro;
     Heading heading;
+    double maxThrust;
+    double mass;
+    double throttle;
+    double fuelConsumed;
+    double fuelRate;
 };
 
 #endif
